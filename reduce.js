a017cd111b70fb9cdc55c9e42903ccd4e35d000c
Arcade Kakpo <arcadej@hotmail.fr>
2024-07-08 10:12:03 +0000

    Group by a property and total their experience

--- reduce.js
+++ reduce.js
@@ -48,3 +48,15 @@ let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience
 console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {Developper: 12, Designer: 4} <-- this is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
