25aa14063bdb6dc4d2eee321f6ad600d80160cf2
Arcade Kakpo <arcadej@hotmail.fr>
2024-07-08 09:32:23 +0000

    Log the total of experience of the members

--- reduce.js
+++ reduce.js
@@ -45,5 +45,6 @@ const teamMembers = [
 
 // Totaling a specific object property
 let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
